27c1bc5e68d7f5133ccc0ca2c39bb36fbdbc2b05
thegreatchrispy <thegreatchrispy@gmail.com>
2018-02-26 22:17:21 -0600

    updated PlayerTest.java

--- tst/com/newmonopoly/PlayerTest.java
+++ tst/com/newmonopoly/PlayerTest.java
@@ -1,3 +1,12 @@
+package com.newmonopoly;
+import org.junit.*;
+
 /**
  * This will be the tests for the Player class.
  */
+public class PlayerTest {
+	@Test
+	public void Test () {
+		Assert.assertTrue(true);
+	}
+}
