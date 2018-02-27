8f80ed54044b13ef8a4077ae1a0ce419e0a1a81e
Isaac Peralez <ipperalez@gmail.com>
2018-02-26 21:53:29 -0600

    Create/Join page button created.

--- rsrc/create_join.js
+++ rsrc/create_join.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     $(".dropdown-toggle").dropdown();
+
+    $('.choice').click(function(){
+    	var id = $(this).attr("rel");
+
+		if($('#join').is(":visible")) {
+			$('#join').hide();
+		}
+
+		if($('#create').is(":visible")) {
+			$('#create').hide();
+		}
+
+    	$('#'+id).show();
+	});
 });
+
