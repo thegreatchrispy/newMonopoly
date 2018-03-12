5fefdf54ece8ec7edf9cb5b56299ba442f58f952
Isaac Peralez <ipperalez@gmail.com>
2018-03-12 16:53:21 -0500

    Update to Gameplay HTML. Swap function created in JavaScript.

---
+++ src/main/resources/static/js/swap.js
@@ -0,0 +1,70 @@
+﻿function swap(swap_1, swap_2) {
+  // Declaring variables.
+  var element_1 = document.getElementById(swap_1);
+  var element_1_class = element_1.className;
+  var element_2 = document.getElementById(swap_2);
+  var element_2_class = element_2.className;
+
+  // Swap
+  element_1.className = element_2_class;
+  element_2.className = element_1_class;
+}
+
+// window.onload = function() {
+//   var test_1 = "group_1";
+//   var test_2 = "group_2";
+
+//   swap(test_1, test_2);
+// };
+
+//////////////////////////////////
+
+
+// $(document).ready(function(){
+//   $(".center").style.background = url(fall.jpg);
+// });
+
+/*$(function() {
+  
+  
+  
+});*/
+//document.getElementsByClassName("center").style.backgroundImage = "url('fall.jpg')";
+// function loopImageFunction(){
+//   var c = document.getElementById("center");
+//   images = ["spring.jpg","summer.jpg","fall.jpg","winter.jpg"];
+//   i=0;
+
+//   setInterval(function(){
+//     if (i === images.length) i=0;
+//     c.style.background = "url(../images/"+images[i]+")";
+//     c.style.backgroundSize = "50%";
+//     i++;
+//   }, 3000);
+// }
+/*
+$(document).ready(function(){
+  $(".center").style.background = url(fall.jpg);
+});
+
+var wallpapers = [
+  'spring.jpg','summer.jpg','fall.jpg','winter.jpg'
+];
+var fullBgElement = $$('.center').first();
+var i = 0;
+window.setInterval(function() {
+  fullBgElement.setStyle({
+    'background': 'url(' + wallpapers[i] + ')'
+  });
+  if (i === (wallpapers.length -1)) {
+    i = 0;
+  }
+  i++;  
+}, 3000);
+
+var bg = $('.center').css('background-image');
+$('.center').fadeOut(3000,function() {
+        $('.center').css({
+            'background-image' : "url('fall.jpg')"
+        });
+$('.center').fadeIn(3000);*/
