a5332d7085c0851e55af5126b2729c29a9121e82
thegreatchrispy <thegreatchrispy@gmail.com>
2018-03-13 13:11:40 -0500

    Deleted all files to update to new branch

--- src/main/java/com/newmonopoly/ObjectDAO.java
+++
@@ -1,9 +0,0 @@
-package com.newmonopoly;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ObjectDAO extends CrudRepository<Player, Long> {
-	List<Player> findByUsername(String username);
-}
