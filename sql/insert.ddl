b68de5171a5af2e3f697410bcfb32c6aade58d18
thegreatchrispy <thegreatchrispy@gmail.com>
2018-03-13 14:56:22 -0500

    Separated Player into two classes: Account.java and Player.java

--- sql/insert.ddl
+++ sql/insert.ddl
@@ -1,4 +1,4 @@
-insert into players(email, username, password, is_admin) values ('cwallen@mail.com', 'hello', 'password', false);
-insert into players(email, username, password, is_admin) values ('chrispy@mail.com', 'crisp', 'password', false);
-insert into players(email, username, password, is_admin) values ('newone@mail.com', 'newx', 'password', false);
-insert into players(email, username, password, is_admin) values ('pokemon@mail.com', 'pokemaster', 'password', true);
+insert into accounts(email, username, password, is_admin) values ('cwallen@mail.com', 'hello', 'password', false);
+insert into accounts(email, username, password, is_admin) values ('chrispy@mail.com', 'crisp', 'password', false);
+insert into accounts(email, username, password, is_admin) values ('newone@mail.com', 'newx', 'password', false);
+insert into accounts(email, username, password, is_admin) values ('pokemon@mail.com', 'pokemaster', 'password', true);
---
+++ src/main/java/com/newmonopoly/Account.java
@@ -0,0 +1,87 @@
+package com.newmonopoly;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="accounts")
+public class Account implements Serializable {
+
+	private static final long serialVersionUID = -3009157732242241606L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "is_admin")
+	private boolean isAdmin;
+
+	protected Account() {
+	}
+
+	public Account(String email, String username, String password, boolean isAdmin) {
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.isAdmin = isAdmin;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Customer{id=%d, email=%s, username=%s, password=%s, is_admin=%b}", id, email, username, password, isAdmin);
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+}
