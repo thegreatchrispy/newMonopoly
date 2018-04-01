2410e1fbbec68df0ac1e066d362a3065e6a7a13e
Chris Allen <thegreatchrispy@users.noreply.github.com>
2018-03-31 23:19:53 -0500

    Update NewMonopolyTest.java

--- test/main/java/com/newmonopoly/NewMonopolyTest.java
+++ test/main/java/com/newmonopoly/NewMonopolyTest.java
@@ -1,28 +1,48 @@
 import com.newmonopoly.Die;
+import com.newmonopoly.Player;
 
 import org.junit.*;
 
+import java.util.List;
+import java.util.Vector;
+
 public class NewMonopolyTest {
-	private NewMonopoly newmonopoly;
+	private NewMonopoly newMonopoly;
+	private List<Player> players;
 
 	public NewMonopolyTest() {
-		newmonopoly = new NewMonopoly();
-		
+		players = new Vector<Player>();
+		players.add(0, new Player("Isaac"));
+		players.add(1, new Player("Juan"));
+		players.add(2, new Player("Chris"));
+		players.add(3, new Player("Ivan"));
+		players.add(4, new Player("Pepe"));
+		players.add(5, new Player("Tang"));
+
+		try {
+			newMonopoly = new NewMonopoly();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
-	public void testNewMonopoly(List<Player> players) {
+	public void testConstructor() {
 		Assert.assertTrue(true);
 	}
 
-	public void testStartGame(){
+	@Test
+	public void testStartGame() {
 		Assert.assertTrue(true);
 	}
 
-	public void testChoosePieces(){
+	@Test
+	public void testChoosePieces() {
 		Assert.assertTrue(true);
 	}
 
-	public void testPlayerOrder(){
+	@Test
+	public void testPlayerOrder() {
 		Assert.assertTrue(true);
 	}
