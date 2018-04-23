99e27beb6fb63eaad72281a45de904b45fe2627a
mapa815 <jmarcelogc85@gmail.com>
2018-04-23 13:19:55 -0500

    modals updated

---
+++ src/main/resources/static/js/player-info.js
@@ -0,0 +1,14 @@
+﻿function player_info(btnID, message, color){
+	var modal = document.getElementById('myModal');
+	var captionText = document.getElementById("PlayerInfo");
+	var btn = document.getElementById(btnID);
+	btn.onclick = function() {
+		modal.style.background = "rgba("+color+",0.9)";
+	    modal.style.display = "block";
+	    captionText.innerHTML = message;
+	}
+	var closeMark = document.getElementById('close1');
+	closeMark.onclick = function() {
+	    modal.style.display = "none";
+	}
+}
