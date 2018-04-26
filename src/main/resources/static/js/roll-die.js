0a4268bcaaaec9d4a799e207cb0264dacd3830fd
mapa815 <jmarcelogc85@gmail.com>
2018-04-25 23:36:12 -0500

    die boxes added

---
+++ src/main/resources/static/js/roll-die.js
@@ -0,0 +1,20 @@
+﻿function rollDie(){
+	var button = document.getElementById('dieButton');
+	var content1 = document.getElementById('dice1');
+	var content2 = document.getElementById('dice2');
+
+	button.onclick = function() {
+	  var result1 = dice.roll();
+	  var result2 = dice.roll();
+	  content1.innerHTML = result1;
+	  content2.innerHTML = result2;
+	};
+
+	var dice = {
+	  sides: 6,
+	  roll: function () {
+	    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+	    return randomNumber;
+	  }
+	}
+}
